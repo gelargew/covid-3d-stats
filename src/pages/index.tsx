ef732bc22bef0a99cfa5bae2cc1fd8bbd768463e
unknown <gelargew@gmail.com>
2022-01-24 15:53:21 +0700

    static props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,6 +31,7 @@ export default function Home({ jsonData }: InferGetStaticPropsType<typeof getSta
 
     return (
         <main>
+            <BarChart data={getNewCasesList(cases)} />
         </main>
     )
 }
