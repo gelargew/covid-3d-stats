af00b83c9c4f18870a7fc3e6591bdd59b9f613fd
unknown <gelargew@gmail.com>
2022-01-24 16:26:25 +0700

    static props

--- src/components/TimeSeriesChart.tsx
+++ src/components/TimeSeriesChart.tsx
@@ -1,8 +1,9 @@
+import { useEffect, useState } from "react";
 import { CasesType } from "../types";
 import { getNewCasesArray } from "../utils/toArray";
 import BarChart from "./BarChart";
 
 export default function TimeSeriesChart({ data }: { data: CasesType}) {
-    console.log(data)
-    return <h1>asds</h1>
+    if (!data) return <h1>asdsad</h1>
+    return <BarChart data={getNewCasesArray(data)} />
 }
