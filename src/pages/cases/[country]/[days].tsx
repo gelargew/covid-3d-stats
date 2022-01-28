fc4d438829ca7d71b6c48d047be4a876c0202157
unknown <gelargew@gmail.com>
2022-01-29 01:01:47 +0700

    add countries as static path

---
+++ src/pages/cases/[country]/[days].tsx
@@ -0,0 +1,45 @@
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next";
+import { useRouter } from "next/router";
+import { ParsedUrlQuery } from "querystring";
+import countries from '../../../countries.json'
+
+export default function Days({ country, days }: InferGetStaticPropsType<typeof getStaticProps>) {
+
+    return (
+        <main>
+            <h1>{country}</h1>
+            <h1>{days}</h1>
+        </main>
+    )
+}
+
+interface Props {
+    country: string,
+    days: string
+}
+
+interface Params extends ParsedUrlQuery {
+    country: string,
+    days: string
+}
+
+export const getStaticProps: GetStaticProps<Props, Params> = async (context) => {
+    const params = context.params!
+    const { country, days } = params
+    return {
+        props: { country, days }
+    }
+}
+
+export const getStaticPaths: GetStaticPaths<Params> = async () => {
+    const paths = []
+    for (const country of countries) {
+        paths.push(...[
+            { params: { country, days: '30' }},
+            { params: { country, days: '90' }},
+            { params: { country, days: '180' }}
+        ])
+    }
+
+    return { paths, fallback: true }
+}
