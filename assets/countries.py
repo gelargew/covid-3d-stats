c310b6fa6217f6290babf9dc22ccca3e77eef23d
unknown <gelargew@gmail.com>
2022-02-01 22:54:32 +0700

    parsing iso3 codes

--- assets/countries.py
+++ assets/countries.py
@@ -7,15 +7,15 @@ import json
 data = requests.get('https://disease.sh/v3/covid-19/countries')
 data = json.loads(data.text)
 df = pd.json_normalize(data)
-countries = df['country'].to_list()
-countries = list(dict.fromkeys(countries))
+ls = df.loc[:,['country', 'countryInfo.iso3', 'countryInfo.lat', 'countryInfo.long', 'countryInfo.flag']]
+ls = ls.values.tolist()
 
 
 
 
 # %%
 with open("countries.json", "w") as f:
-    data = json.dumps(countries)
+    data = json.dumps(ls)
     f.write(data)
 
 # %%
