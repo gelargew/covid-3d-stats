a4926d712ef0de647857b12b66db99f26888a847
unknown <gelargew@gmail.com>
2022-02-11 23:05:05 +0700

    object rotation & position

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -1,5 +1,5 @@
 import { Box, Instance, Instances, MapControls, MeshReflectorMaterial, OrbitControls, Plane, SpotLight, TrackballControls } from "@react-three/drei"
-import { Canvas, Props, useFrame, useThree } from "@react-three/fiber"
+import { Canvas, Props, ThreeEvent, useFrame, useThree } from "@react-three/fiber"
 
 import { Suspense, useEffect, useLayoutEffect, useMemo, useRef, useState } from "react"
 import * as THREE from 'three'
@@ -9,28 +9,30 @@ interface PropsType {
     count: number
 }
 
+interface chartProps {
+    name?: string,
+    data: PropsType[],
+    position?: [x: number, y: number, z: number],
+    rotation?: [x: number, y: number, z: number, order?: string | undefined]
+}
+
 const color = new THREE.Color()
 
-export default function BarChart({ data, position=[0, 0, 0] }: { data: PropsType[], position?: [x: number, y: number, z: number]}) {
+
+export default function BarChart({ name='sd', data, position=[0,0,0], rotation=[0,0,0] }: chartProps) {
     const height = Math.max(...data.map(d => d.count))/50
     const width = useMemo(() => 50/data.length, [])
     const geometry = new THREE.BoxGeometry(width, 1, width)
-    const {camera} = useThree()
     geometry.translate(0, 0.5, -0.5)
-    const handleClick = () => {
-        console.log('click')
-    }
 
-    useEffect(() => {
-        camera.position.set(1, 5, 40)
-    },[])
+
+
 
     return (
-        <group position={position}>
-            
+        <group scale={0.1} position={position} rotation={rotation}  >          
             <Suspense fallback={null}>
-                <Instances geometry={geometry} limit={100000} range={100000} onClick={handleClick} >
-                    <meshLambertMaterial />
+                <Instances geometry={geometry} limit={100000} range={100000} name={name}  >
+                    <meshLambertMaterial  />
                     {data.map((d, i) => 
                     <Bar key={i} 
                     position={[(i - data.length/2)*1.1*width, 0, 0]} 
@@ -49,17 +51,18 @@ const Bar = ({data, height, ...props}: any) => {
     const zScale = data.count/height/10
     const [hovered, setHovered] = useState(false)
 
-    useFrame(() => {
+    useFrame((st, dt) => {
         ref.current.scale.y = THREE.MathUtils.damp(ref.current.scale.y, yScale, 0.01, 5)
         ref.current.scale.z = THREE.MathUtils.damp(ref.current.scale.z, zScale, 0.01, 5)
-        ref.current.color.lerp(color.set(hovered ? 'red' : 'lightblue'), hovered ? 1 : 0.1)
+        ref.current.color.lerp(color.set(hovered ? 'red' : 'blue'), hovered ? 1 : 0.1)
     })
 
 
     return (
-        <Instance ref={ref} color='lightblue' castShadow
+        <Instance ref={ref} color={'blue'} castShadow
         onPointerOver={() => setHovered(true)}
         onPointerLeave={() => setHovered(false)} 
+        name={data.title}
         {...props} />
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,18 @@
 import { GetServerSideProps, GetStaticProps, InferGetServerSidePropsType, InferGetStaticPropsType } from "next"
-import { useEffect, useState } from "react"
+import { ReactNode, RefObject, useEffect, useRef, useState } from "react"
 import BarChart from '../components/BarChart'
 import { CasesType, NewCasesType, TimeSeriesType } from '../types'
 import { getNewCasesArray } from '../utils/toArray'
-import { Canvas, useFrame, useThree } from '@react-three/fiber'
+import { Canvas, ThreeEvent, useFrame, useThree } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import { Reflector } from "../components/Reflector"
 import * as THREE from 'three'
+import { useRouter } from "next/router"
+
+import historical from '../historical.json'
 
 
-
+const GOLDENRATIO = 1.61803398875
 
 export default function Home({ jsonData }: InferGetStaticPropsType<typeof getStaticProps>) {
     const data: TimeSeriesType = jsonData
@@ -18,10 +21,11 @@ export default function Home({ jsonData }: InferGetStaticPropsType<typeof getSta
         <>
             <main>
                 <Canvas>
-
                     <ambientLight />
                     <Objects data={data} />
+                    <Reflector />
                 </Canvas>
+                <button id='back'>BACK</button>
             </main>
         </>
         
@@ -31,27 +35,44 @@ export default function Home({ jsonData }: InferGetStaticPropsType<typeof getSta
 const Objects = ({ data }: { data: TimeSeriesType}) => {
     const casesData = getNewCasesArray(data.cases)
     const deathsData = getNewCasesArray(data.deaths)
-    const {camera} = useThree()
-    const q = new THREE.Quaternion(100, 55, 100, 5)
+    const p = new THREE.Vector3(0, 30, 60)
+    const q = new THREE.Quaternion(0.4, 0, 0)
+    const ref = useRef<THREE.Group>(null!)
 
-    useFrame((state, dt) => {
-        state.camera.quaternion.slerp(q, dt)
+    
+    const handleClick = (e: ThreeEvent<MouseEvent>) => {
+        e.stopPropagation()
+        e.object.parent?.updateWorldMatrix(true, true)
+        e.object.parent?.localToWorld(p.set(0, 20, 40))
+        e.object.parent?.getWorldQuaternion(q)
+    }
+
+    useEffect(() => {
+        ref.current.getWorldQuaternion(q)
+        console.log(q)
+    }, [])
+
+    useFrame((state, delta) => {
+        /* state.camera.position.lerp(p, THREE.MathUtils.damp(0, 1, 3, delta))
+        state.camera.quaternion.slerp(q, THREE.MathUtils.damp(0, 1, 2, delta)) */
+        state.camera.quaternion.slerp(q, THREE.MathUtils.damp(0, 1, 2, delta))
+        state.camera.position.lerp(p, THREE.MathUtils.damp(0, 1, 3, delta))
     })
 
 
     return (
-        <>
-            <BarChart data={casesData} />
-            <BarChart data={deathsData} position={[100, 0, 0]}/>
-            <Reflector />
-        </>
+        <group ref={ref} onClick={handleClick}>
+            <BarChart name='cases' data={casesData} position={[0, 0, 10]} />
+            <BarChart name='deaths' data={deathsData} position={[20, 0, 0]} rotation={[0, Math.PI/4, 0]} />
+        </group>
     )
 }
 
 
 const getStaticProps: GetStaticProps = async () => {
-    const res = await fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=15')
-    const jsonData: TimeSeriesType = await res.json()
+    /* const res = await fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
+    const jsonData: TimeSeriesType = await res.json() */
+    const jsonData: TimeSeriesType = historical
 
     return { props: { jsonData }, revalidate: 60*60*24 }
 }
