5be8f0c22e54551ac95784bc8df9ac4e97d084f2
unknown <gelargew@gmail.com>
2022-02-22 17:22:06 +0700

    separate data for each country

--- covid_data/historical_all.py
+++ covid_data/historical_all.py
@@ -11,27 +11,26 @@ with open(os.path.join(BASE_DIR, 'covid_data', 'countries_info.json'), 'r') as f
     countries = json.load(f)
 
 ls = ','.join([country for country in countries])
-url = f'https://disease.sh/v3/covid-19/historical/{ls}?lastdays=360'
+url = f'https://disease.sh/v3/covid-19/historical/{ls}?lastdays=30'
 data = requests.get(url)
 data = json.loads(data.text)
 
 # %%
-historical_all = {}
 for country in data:    
     if country:
         try:
             country_name = country['country']
-            historical_all[country_name] = {
+            output = {
                 'cases': country['timeline']['cases'],
                 'deaths': country['timeline']['deaths']
             }
+            filename = countries[country_name]['slug']
+            with open(os.path.join(BASE_DIR, 'covid_data', 'historical_all', f'{filename}.json'), 'w') as f:
+                json.dump(output, f, indent=4)
         except (ValueError, KeyError):
             continue       
 
 
-# %%
-with open(os.path.join(BASE_DIR, 'covid_data', 'historical_all.json'), 'w') as f:
-    json.dump(historical_all, f, indent=4)
 
 
 
